330a485a92ae3e766f3bd8e218d5219e4fb23d63
AlexandraUrsu5 <alexaursu05@gmail.com>
2022-11-27 18:44:54 +0200

    added some unit tests

---
+++ MyDocAppointment.Tests/DoctorControllerShould.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MyDocAppointment.API.Controllers;
+using MyDocAppointment.API.Dtos;
+using MyDocAppointment.Business.Helpers;
+using MyDocAppointment.Business.Interfaces;
+using MyDocAppointment.Business.Users;
+using Xunit;
+
+namespace MyDocAppointment.Tests
+{
+    public class DoctorControllerShould
+    {
+        DoctorController controller;
+        Guid idOk;
+        Guid idNotFound;
+        public DoctorControllerShould()
+        {
+            // Arrange
+            idOk = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+            idNotFound = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa5");
+            var doctorsServiceMock = new Mock<IDoctorsService>();
+            doctorsServiceMock.Setup(r => r.GetById(idOk))
+                .ReturnsAsync(GetTestDoctors());
+            doctorsServiceMock.Setup(r => r.GetById(idNotFound))
+                .Returns(FailureResult(idNotFound));
+            doctorsServiceMock.Setup(r => r.Create(It.IsAny<Doctor>()))
+                .Returns(Task.CompletedTask);
+            doctorsServiceMock.Setup(r => r.Delete(It.IsAny<Guid>()))
+                 .Returns(SuccessResult());
+            controller = new DoctorController(doctorsServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsOk_WhenTheDoctorWithSpecifiedIdIsFound()
+        {
+            //Act
+            var response = await controller.GetById(idOk);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(response);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsNotFound_WhenTheDoctorWithSpecifiedIdIsNotFound()
+        {
+            //Act
+            var response = await controller.GetById(idNotFound);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(response);
+        }
+
+        [Fact]
+        public async Task Create_ReturnsCreatedResult()
+        {
+            //Act
+            var response = await controller.Create(GetDoctorDto());
+
+            // Assert
+            Assert.IsType<CreatedResult>(response);
+        }
+
+        [Fact]
+        public async Task Delete_ReturnsOk_WhenTheDoctorWithSpecifiedIdIsFound()
+        {
+            //Act
+            var response = await controller.Delete(idOk);
+
+            // Assert
+            Assert.IsType<OkResult>(response);
+        }
+
+        private CreateDoctorDto GetDoctorDto()
+        {
+
+            var doctor = new CreateDoctorDto();
+            return doctor;
+
+        }
+        private Result<Doctor> GetTestDoctors()
+        {
+            var doctor = new Doctor();
+            return Result<Doctor>.Success(doctor);
+        }
+        private async Task<Result> SuccessResult()
+        {
+            return Result.Success();
+        }
+        private async Task<Result<Doctor>> FailureResult(Guid id)
+        {
+            return Result<Doctor>.Failure($"Patient with ID: {id} does not exist.");
+        }
+    }
+}
---
+++ MyDocAppointment.Tests/PatientControllerShould.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MyDocAppointment.API.Controllers;
+using MyDocAppointment.API.Dtos;
+using MyDocAppointment.Business.Helpers;
+using MyDocAppointment.Business.Interfaces;
+using MyDocAppointment.Business.Users;
+using Xunit;
+
+namespace MyDocAppointment.Tests
+{
+    public class PatientControllerShould
+    {
+        PatientController controller;
+        Guid idOk;
+        Guid idNotFound;
+        public PatientControllerShould()
+        {
+            // Arrange
+            idOk = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+            idNotFound = new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa5");
+            var patientsServiceMock = new Mock<IPatientsService>();
+            patientsServiceMock.Setup(r => r.GetById(idOk))
+                .ReturnsAsync(GetTestPatients());
+            patientsServiceMock.Setup(r => r.GetById(idNotFound))
+                .Returns(FailureResult(idNotFound));
+            patientsServiceMock.Setup(r => r.Create(It.IsAny<Patient>()))
+                .Returns(Task.CompletedTask);
+            patientsServiceMock.Setup(r => r.Delete(It.IsAny<Guid>()))
+                 .Returns(SuccessResult());
+            controller = new PatientController(patientsServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsOk_WhenThePatientWithSpecifiedIdIsFound()
+        {
+            //Act
+            var response = await controller.GetById(idOk);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(response);
+        }
+        [Fact]
+        public async Task GetById_ReturnsNotFound_WhenThePatientWithSpecifiedIdIsNotFound()
+        {
+            //Act
+            var response = await controller.GetById(idNotFound);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(response);
+        }
+
+        [Fact]
+        public async Task Create_ReturnsCreatedResult()
+        {
+            //Act
+            var response = await controller.Create(GetPatientDto());
+
+            // Assert
+            Assert.IsType<CreatedResult>(response);
+        }
+
+        [Fact]
+        public async Task Delete_ReturnsOk_WhenThePatientWithSpecifiedIdIsFound()
+        {
+            //Act
+            var response = await controller.Delete(idOk);
+
+            // Assert
+            Assert.IsType<OkResult>(response);
+        }
+
+        private CreatePatientDto GetPatientDto()
+        {
+
+            var patient = new CreatePatientDto();
+            return patient;
+
+        }
+        private Result<Patient> GetTestPatients()
+        {
+            var patient = new Patient();
+            return Result<Patient>.Success(patient);
+        }
+        private async Task<Result> SuccessResult()
+        {
+            return Result.Success();
+        }
+        private async Task<Result<Patient>> FailureResult(Guid id)
+        {
+            return Result<Patient>.Failure($"Patient with ID: {id} does not exist.");
+        }
+    }
+}
