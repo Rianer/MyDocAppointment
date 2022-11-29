1cf297de78c2cd0e59f6165bf403b4a7ca4b32c1
Rianer <aerhan2001@gmail.com>
2022-11-29 12:28:28 +0200

    Service route

--- Frontend/my-doc-appointment/src/app/app-routing.module.ts
+++ Frontend/my-doc-appointment/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:'services',
+  component:ServicesComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
