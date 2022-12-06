f625e98ac051c9094d79be7a062ff4093318113d
RaulPetcu <79147895+RaulPetcu@users.noreply.github.com>
2022-12-06 13:28:14 +0200

    .

---
+++ MyDocAppointment.Tests/IntegrationTests/MyDocAppointment.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace MyDocAppointment.Tests.Integration
+{
+
+    public class DoctorControllerIntegrationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
